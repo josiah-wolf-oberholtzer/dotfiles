95ba337367b23312d9ef265a12fb32ac484ec94e
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2018-07-26 22:29:18 -0400

    Cleanup

--- config/.git-prompt-colors.sh
+++ config/.git-prompt-colors.sh
@@ -7,40 +7,40 @@
 
 define_helpers() {
     YELLOW=$(tput setaf 3)
-   PathShort="\[$YELLOW\][\A] ${Cyan}\w"
+    PathShort="\[$YELLOW\][\A] ${Cyan}\w"
 }
 
 override_git_prompt_colors() {
-   YELLOW=$(tput setaf 3)
-   ORANGE=$(tput setaf 9)
-   RED=$(tput setaf 1)
-   MAGENTA=$(tput setaf 5)
-   VIOLET=$(tput setaf 13)
-   BLUE=$(tput setaf 4)
-   CYAN=$(tput setaf 6)
-   GREEN=$(tput setaf 2)
-   BOLD=$(tput bold)
-   RESET=$(tput sgr0)
+    YELLOW=$(tput setaf 3)
+    ORANGE=$(tput setaf 9)
+    RED=$(tput setaf 1)
+    MAGENTA=$(tput setaf 5)
+    VIOLET=$(tput setaf 13)
+    BLUE=$(tput setaf 4)
+    CYAN=$(tput setaf 6)
+    GREEN=$(tput setaf 2)
+    BOLD=$(tput bold)
+    RESET=$(tput sgr0)
 
-   GIT_PROMPT_THEME_NAME="Single_line_Solarized_Lamda"
+    GIT_PROMPT_THEME_NAME="Single_line_Solarized_Lamda"
 
-   GIT_PROMPT_PREFIX="[ "
-   GIT_PROMPT_SUFFIX=" ]"
-   GIT_PROMPT_SEPARATOR=" |"
-   GIT_PROMPT_STAGED=" ${Yellow}● ${ResetColor}"
-   GIT_PROMPT_CONFLICTS=" ${Red}:heavy_multiplication_x: ${ResetColor}"
-   GIT_PROMPT_CHANGED=" ${Blue}✚ ${ResetColor}"
-   GIT_PROMPT_UNTRACKED=" ${Cyan}…${ResetColor}"
-   GIT_PROMPT_STASHED=" ${BoldMagenta}⚑ ${ResetColor}"
-   GIT_PROMPT_CLEAN=" ${Green}:heavy_check_mark: ${ResetColor}"
+    GIT_PROMPT_PREFIX="[ "
+    GIT_PROMPT_SUFFIX=" ]"
+    GIT_PROMPT_SEPARATOR=" |"
+    GIT_PROMPT_STAGED=" ${Yellow}● ${ResetColor}"
+    GIT_PROMPT_CONFLICTS=" ${Red}✖ ${ResetColor}"
+    GIT_PROMPT_CHANGED=" ${Blue}✚ ${ResetColor}"
+    GIT_PROMPT_UNTRACKED=" ${Cyan}…${ResetColor}"
+    GIT_PROMPT_STASHED=" ${BoldMagenta}⚑ ${ResetColor}"
+    GIT_PROMPT_CLEAN=" ${Green}✔ ${ResetColor}"
 
-   GIT_PROMPT_COMMAND_FAIL="${Red}✘"
+    GIT_PROMPT_COMMAND_FAIL="${Red}✘"
 
-   GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${Yellow}${PathShort}${ResetColor}"
-   GIT_PROMPT_END_USER="\[$ORANGE\]\nλ ${ResetColor}"
-   GIT_PROMPT_END_ROOT="${BoldRed}\n# ${ResetColor}"
+    GIT_PROMPT_START_USER="_LAST_COMMAND_INDICATOR_ ${Yellow}${PathShort}${ResetColor}"
+    GIT_PROMPT_END_USER="\[$ORANGE\]\nλ ${ResetColor}"
+    GIT_PROMPT_END_ROOT="${BoldRed}\n# ${ResetColor}"
 
-   GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="✭"
+    GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="✭"
 }
 
 reload_git_prompt_colors "Custom"
